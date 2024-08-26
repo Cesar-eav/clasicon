708faaf6d633513e11db00e5e353fbee06b4b5f7
Cesar-eav <cesar.eav@hotmail.es>
2024-08-25 21:17:19 -0400

    Sin barra buscadora

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -45,19 +45,21 @@ onUnmounted(() => {
         <div class="flex items-center gap-2">
 
         </div>
-        <div class="flex items-center gap-2">
+        <!-- <div class="flex items-center gap-2">
             <Button iconOnly variant="secondary" type="button" @click="() => { toggleDarkMode() }"
                 v-slot="{ iconSizeClasses }" class="md:hidden" srText="Toggle dark mode">
                 <MoonIcon v-show="!isDark" aria-hidden="true" :class="iconSizeClasses" />
                 <SunIcon v-show="isDark" aria-hidden="true" :class="iconSizeClasses" />
             </Button>
-        </div>
+        </div> -->
 
         <!-- Search Icon and Input -->
-        <div class="flex items-center gap-2">
-            <SearchIcon aria-hidden="true" class="w-5 h-5 text-gray-500" />
+        <div class="flex flex-col items-left gap-2">
+            <!-- <p class="text-gray-600 font-bold">{{ $page.props.auth.user.name }}</p> -->
+            <p class="text-gray-600 font-bold">¡Recomienda tu clasicón!</p>
+            <!-- <SearchIcon aria-hidden="true" class="w-5 h-5 text-gray-500" />
             <input type="text" placeholder="Buscar tu clasicón"
-                class="px-3 py-2 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-dark-eval-2 dark:text-gray-300" />
+                class="px-3 py-2 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-dark-eval-2 dark:text-gray-300" /> -->
         </div>
         <div class="flex items-center gap-2">
             <Button iconOnly variant="secondary" type="button" @click="() => { toggleDarkMode() }"
