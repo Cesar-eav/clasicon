fd71103d77bd78809ac0289d4671a0ea1feba862
Cesar-eav <cesar.eav@hotmail.es>
2024-08-30 17:50:00 -0400

    Nuevo diseño 2

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -48,31 +48,31 @@ shuffleRecommendations();
         <!-- Fondo oscuro transparente -->
         <div class="absolute inset-0 bg-gradient-to-r from-black via-transparent to-black opacity-80"></div>
 
-        <div class="relative z-10 flex flex-col md:flex-row w-full max-w-screen-xl mx-auto">
+        <div class="relative z-10 flex flex-col md:flex-row w-full max-w-screen-xl mx-auto px-4 md:px-0">
             <!-- Sección principal -->
             <div class="flex-1 text-center md:text-left p-6">
-                <h1 class="text-5xl md:text-7xl font-extrabold mb-4 tracking-tight leading-tight">Bienvenido a <span class="text-blue-500">Clasicón.com</span></h1>
-                <p class="text-lg md:text-xl mb-8">Descubre, comparte y recomienda lo mejor en libros, películas, series y mucho más. Únete a una comunidad apasionada por el conocimiento y el entretenimiento.</p>
+                <h1 class="text-4xl md:text-7xl font-extrabold mb-4 tracking-tight leading-tight">Bienvenido a <span class="text-blue-500">Clasicón.com</span></h1>
+                <p class="text-base md:text-xl mb-6 md:mb-8">Descubre, comparte y recomienda lo mejor en libros, películas, series y mucho más. Únete a una comunidad apasionada por el conocimiento y el entretenimiento.</p>
 
                 <!-- Botones de inicio de sesión y registro -->
-                <div class="flex flex-col md:flex-row items-center gap-4 mt-6 md:mt-12">
+                <div class="flex flex-col md:flex-row items-center gap-4 mt-4 md:mt-12">
                     <Link v-if="!auth.user" href="/login"
-                        class="bg-gradient-to-r from-blue-500 to-purple-600 w-56 h-12  text-white px-6 py-2 rounded-full hover:bg-blue-700 flex items-center justify-center gap-2 transition-transform transform hover:scale-105">
+                        class="bg-gradient-to-r from-blue-500 to-purple-600 w-full md:w-56 h-12 text-white px-6 py-2 rounded-full hover:bg-blue-700 flex items-center justify-center gap-2 transition-transform transform hover:scale-105">
                     <FontAwesomeIcon :icon="faSignInAlt" />
                     Ingresar
                     </Link>
 
                     <Link v-if="!auth.user" :href="route('register')"
-                        class="bg-gradient-to-r from-green-500 to-teal-500 w-56 h-12 text-white px-6 py-2 rounded-full hover:bg-green-700 flex items-center justify-center gap-2 transition-transform transform hover:scale-105">
+                        class="bg-gradient-to-r from-green-500 to-teal-500 w-full md:w-56 h-12 text-white px-6 py-2 rounded-full hover:bg-green-700 flex items-center justify-center gap-2 transition-transform transform hover:scale-105">
                     <FontAwesomeIcon :icon="faUserPlus" />
                     Registrarse
                     </Link>
                 </div>
 
                 <!-- Enlace para explorar sin iniciar sesión -->
-                <div v-if="auth.user" class="mt-12">
+                <div v-if="auth.user" class="mt-6 md:mt-12">
                     <Link :href="route('dashboard')"
-                        class="bg-gray-800 text-white px-6 py-2 rounded-full hover:bg-gray-900 flex items-center justify-center gap-2 transition-transform transform hover:scale-105">
+                        class="bg-gray-800 text-white px-6 py-2 rounded-full hover:bg-gray-900 flex items-center justify-center gap-2 transition-transform transform hover:scale-105 w-full md:w-auto">
                     <FontAwesomeIcon :icon="faCompass" />
                     Explorar
                     </Link>
@@ -81,24 +81,24 @@ shuffleRecommendations();
 
             <!-- Sección de recomendaciones -->
             <div
-                class="w-full md:w-2/3 p-4 md:p-8 bg-gray-900 bg-opacity-90 rounded-lg shadow-lg h-[75vh] flex flex-col backdrop-blur-md">
+                class="w-full md:w-2/3 p-4 md:p-8 bg-gray-900 bg-opacity-90 rounded-lg shadow-lg h-auto md:h-[75vh] flex flex-col backdrop-blur-md mt-8 md:mt-0">
                 <div class="flex items-center justify-between mb-4">
-                    <h2 class="text-3xl font-semibold text-white">Clasicones de hoy</h2>
+                    <h2 class="text-2xl md:text-3xl font-semibold text-white">Clasicones de hoy</h2>
                     <button @click="shuffleRecommendations" class="text-white hover:text-gray-400 transition-transform transform hover:scale-110">
                         <FontAwesomeIcon :icon="faSyncAlt" />
                     </button>
                 </div>
-                <div class="space-y-4 overflow-y-auto">
+                <div class="space-y-4 overflow-y-auto max-h-96 md:max-h-full">
                     <div v-for="(recommendation, index) in recommendations_organic" :key="recommendation.id"
                         class="flex items-center bg-gray-800 bg-opacity-80 rounded-lg p-4 hover:bg-opacity-100 transition-colors">
                         <img :src="`/storage/${recommendation.image}`" alt="Recommendation Image"
-                            class="w-20 h-24 object-cover rounded-md mr-4 shadow-md">
+                            class="w-16 h-20 md:w-20 md:h-24 object-cover rounded-md mr-4 shadow-md">
                         <div class="flex-1">
-                            <h3 class="text-xl font-bold text-white">{{ recommendation.title }}</h3>
-                            <p class="text-sm text-gray-300">
-                                <strong class="text-gray-800 text-2xl">"</strong>
+                            <h3 class="text-lg md:text-xl font-bold text-white">{{ recommendation.title }}</h3>
+                            <p class="text-xs md:text-sm text-gray-300">
+                                <strong class="text-gray-800 text-xl md:text-2xl">"</strong>
                                 {{ isExpanded[index] ? recommendation.description : recommendation.description.slice(0, 350) }}
-                                <strong class="text-gray-800 text-xl">"</strong>
+                                <strong class="text-gray-800 text-lg md:text-xl">"</strong>
                                 <span v-if="recommendation.description.length > 350" @click="toggleExpansion(index)"
                                     class="text-blue-500 cursor-pointer">
                                     {{ isExpanded[index] ? '...ver menos' : '...ver más' }}
@@ -119,4 +119,5 @@ shuffleRecommendations();
         </footer>
     </div>
 </template>
+
 
