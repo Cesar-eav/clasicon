0b2083bf4155b435bab3e14dd1f0bceea359a3ac
Cesar-eav <cesar.eav@hotmail.es>
2024-08-30 17:46:57 -0400

    NUevo diseño

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -46,55 +46,55 @@ shuffleRecommendations();
     <div class="relative flex items-center justify-center min-h-screen bg-cover bg-center text-white"
         :style="{ backgroundImage: `url(${backgroundImage})` }">
         <!-- Fondo oscuro transparente -->
-        <div class="absolute inset-0 bg-black opacity-50"></div>
+        <div class="absolute inset-0 bg-gradient-to-r from-black via-transparent to-black opacity-80"></div>
 
         <div class="relative z-10 flex flex-col md:flex-row w-full max-w-screen-xl mx-auto">
             <!-- Sección principal -->
             <div class="flex-1 text-center md:text-left p-6">
-                <h1 class="text-4xl md:text-6xl font-bold mb-4">Clasicón.com</h1>
-                <p class="text-lg md:text-xl mb-8">Tu espacio para descubrir, compartir y recomendar lo mejor en libros,
-                    películas, series, y más.</p>
+                <h1 class="text-5xl md:text-7xl font-extrabold mb-4 tracking-tight leading-tight">Bienvenido a <span class="text-blue-500">Clasicón.com</span></h1>
+                <p class="text-lg md:text-xl mb-8">Descubre, comparte y recomienda lo mejor en libros, películas, series y mucho más. Únete a una comunidad apasionada por el conocimiento y el entretenimiento.</p>
 
                 <!-- Botones de inicio de sesión y registro -->
-                <div class="flex flex-col items-center gap-4 mt-6 md:mt-20">
+                <div class="flex flex-col md:flex-row items-center gap-4 mt-6 md:mt-12">
                     <Link v-if="!auth.user" href="/login"
-                        class="bg-blue-600 w-56 h-12  text-white px-6 py-2 rounded-full hover:bg-blue-700 flex items-center justify-center gap-2">
-                    <FontAwesomeIcon :icon="faTachometerAlt" />
+                        class="bg-gradient-to-r from-blue-500 to-purple-600 w-56 h-12  text-white px-6 py-2 rounded-full hover:bg-blue-700 flex items-center justify-center gap-2 transition-transform transform hover:scale-105">
+                    <FontAwesomeIcon :icon="faSignInAlt" />
                     Ingresar
                     </Link>
 
                     <Link v-if="!auth.user" :href="route('register')"
-                        class="bg-green-600 w-56 h-12 text-white px-6 py-2 rounded-full hover:bg-green-700 flex items-center justify-center gap-2">
+                        class="bg-gradient-to-r from-green-500 to-teal-500 w-56 h-12 text-white px-6 py-2 rounded-full hover:bg-green-700 flex items-center justify-center gap-2 transition-transform transform hover:scale-105">
                     <FontAwesomeIcon :icon="faUserPlus" />
                     Registrarse
                     </Link>
                 </div>
 
                 <!-- Enlace para explorar sin iniciar sesión -->
-                <div v-if="auth.user" class="mt-8">
+                <div v-if="auth.user" class="mt-12">
                     <Link :href="route('dashboard')"
-                        class="bg-gray-800 text-white px-6 py-2 rounded-full hover:bg-gray-900 flex items-center justify-center gap-2">
+                        class="bg-gray-800 text-white px-6 py-2 rounded-full hover:bg-gray-900 flex items-center justify-center gap-2 transition-transform transform hover:scale-105">
                     <FontAwesomeIcon :icon="faCompass" />
-                    INGRESAR
+                    Explorar
                     </Link>
                 </div>
             </div>
+
             <!-- Sección de recomendaciones -->
             <div
-                class="w-full md:w-2/3 p-2 md:p-6 bg-gray-800 dark:bg-gray-900 rounded-lg shadow-lg h-[75vh] flex flex-col">
+                class="w-full md:w-2/3 p-4 md:p-8 bg-gray-900 bg-opacity-90 rounded-lg shadow-lg h-[75vh] flex flex-col backdrop-blur-md">
                 <div class="flex items-center justify-between mb-4">
-                    <h2 class="text-2xl font-semibold text-white">Clasicones de hoy</h2>
-                    <button @click="shuffleRecommendations" class="text-white hover:text-gray-400">
+                    <h2 class="text-3xl font-semibold text-white">Clasicones de hoy</h2>
+                    <button @click="shuffleRecommendations" class="text-white hover:text-gray-400 transition-transform transform hover:scale-110">
                         <FontAwesomeIcon :icon="faSyncAlt" />
                     </button>
                 </div>
                 <div class="space-y-4 overflow-y-auto">
                     <div v-for="(recommendation, index) in recommendations_organic" :key="recommendation.id"
-                        class="flex items-center bg-gray-700 dark:bg-gray-800 rounded-lg p-4">
+                        class="flex items-center bg-gray-800 bg-opacity-80 rounded-lg p-4 hover:bg-opacity-100 transition-colors">
                         <img :src="`/storage/${recommendation.image}`" alt="Recommendation Image"
-                            class="w-16 h-22 object-cover rounded-md mr-4">
+                            class="w-20 h-24 object-cover rounded-md mr-4 shadow-md">
                         <div class="flex-1">
-                            <h3 class="text-lg font-bold text-white">{{ recommendation.title }}</h3>
+                            <h3 class="text-xl font-bold text-white">{{ recommendation.title }}</h3>
                             <p class="text-sm text-gray-300">
                                 <strong class="text-gray-800 text-2xl">"</strong>
                                 {{ isExpanded[index] ? recommendation.description : recommendation.description.slice(0, 350) }}
@@ -104,10 +104,9 @@ shuffleRecommendations();
                                     {{ isExpanded[index] ? '...ver menos' : '...ver más' }}
                                 </span>
                             </p>
-                            <p class="text-xs text-red-400 flex items-center gap-1">
+                            <p class="text-xs text-red-400 flex items-center gap-1 mt-2">
                              <FontAwesomeIcon :icon="faUser" />   Aporte de {{ recommendation.user.name }}
                             </p>
-
                         </div>
                     </div>
                 </div>
@@ -117,7 +116,7 @@ shuffleRecommendations();
         <!-- Footer con información adicional -->
         <footer class="absolute bottom-0 w-full text-center py-4 bg-black bg-opacity-60 text-gray-300">
             <p>&copy; 2024 Clasicón. Todos los derechos reservados.</p>
-            <!-- <p>Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})</p> -->
         </footer>
     </div>
 </template>
+
