4e4351f8f071062da9d796dfc6ad477e47daf845
Cesar-eav <cesar.eav@hotmail.es>
2024-09-07 14:06:42 -0400

    Fix menu movil

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -126,6 +126,12 @@ const selectRecommendation = (result) => {
                             </svg>
                         </button>
                     </span>
+
+
+
+
+                    
+                    
                 </template>
 
                 <template #content class="z-50">
@@ -135,4 +141,30 @@ const selectRecommendation = (result) => {
             </Dropdown>
         </div>
     </nav>
+
+
+
+      <!-- Mobile bottom bar -->
+      <div :class="[
+        'fixed inset-x-0 bottom-0 flex items-center justify-between px-4 py-4 sm:px-6 transition-transform duration-500 bg-white md:hidden dark:bg-dark-eval-1',
+        {
+            'translate-y-full': scrolling.down,
+            'translate-y-0': scrolling.up,
+        },
+    ]">
+        <Button iconOnly variant="secondary" type="button" v-slot="{ iconSizeClasses }" srText="Search">
+            <SearchIcon aria-hidden="true" :class="iconSizeClasses" />
+        </Button>
+
+        <Link :href="route('dashboard')">
+        <h1 class="text-black font-bold text-2xl">C</h1>
+        <span class="sr-only">C</span>
+        </Link>
+
+        <Button iconOnly variant="secondary" type="button" @click="sidebarState.isOpen = !sidebarState.isOpen"
+            v-slot="{ iconSizeClasses }" class="md:hidden" srText="Search">
+            <MenuIcon v-show="!sidebarState.isOpen" aria-hidden="true" :class="iconSizeClasses" />
+            <XIcon v-show="sidebarState.isOpen" aria-hidden="true" :class="iconSizeClasses" />
+        </Button>
+    </div>
 </template>
