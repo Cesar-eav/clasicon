7640875c865524dad74a23363e5c32fe29be4d6e
Cesar-eav <cesar.eav@hotmail.es>
2024-09-09 09:26:48 -0300

    Hidden movil

--- resources/js/Components/PageFooter.vue
+++ resources/js/Components/PageFooter.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <footer class="flex-shrink-0 px-6 py-4">
+    <footer class="flex-shrink-0 px-6 py-4 hidden md:block">
         <p
             class="flex items-center justify-center gap-1 text-sm text-gray-600  dark:text-gray-400"
         >
