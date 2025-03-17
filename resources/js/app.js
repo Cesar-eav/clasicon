9c07aa28b6e51252ec924d90d0295eaa173e1ea5
Cesar-eav <cesar.eav@hotmail.es>
2025-03-16 21:46:16 -0300

    Iconos para atractivos

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,10 +9,10 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 // Importaciones de Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faBell,faBook, faFilm, faFilter, faTv, faCirclePlay, faFileVideo, faGamepad, faLocationDot ,faMusic, faLandmark, faMapMarkedAlt, faEllipsisH, faPodcast } from '@fortawesome/free-solid-svg-icons';
+import { faBell,faBook, faFilm, faLeaf, faFilter, faTv, faCirclePlay, faFileVideo, faGamepad, faLocationDot ,faMusic, faLandmark, faMapMarkedAlt, faEllipsisH, faPodcast,faArchway, faSprayCan, faBuilding, faMuseum, faUtensils } from '@fortawesome/free-solid-svg-icons';
 
 // Añadir los íconos que necesitas a la librería de Font Awesome
-library.add(faBell,faBook, faFilm,faFilter, faPodcast, faTv,faCirclePlay,faFileVideo,faLocationDot, faGamepad, faMusic, faLandmark, faMapMarkedAlt, faEllipsisH);
+library.add(faBell,faBook, faLeaf, faFilm,faFilter, faPodcast, faTv,faCirclePlay,faFileVideo,faLocationDot, faGamepad, faMusic, faLandmark, faMapMarkedAlt, faEllipsisH,faArchway, faSprayCan, faBuilding, faMuseum, faUtensils);
 
 const appName = import.meta.env.VITE_APP_NAME || 'Clasicón.com';
 
