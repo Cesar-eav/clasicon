d62bed5a5bb52973f64ca4fadf234651dc2fba4c
Cesar-eav <cesar.eav@hotmail.es>
2025-03-16 21:45:18 -0300

    Nuevas Categorias

--- resources/js/Pages/Recommendations/Create.vue
+++ resources/js/Pages/Recommendations/Create.vue
@@ -3,7 +3,7 @@ import AuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import { ref } from 'vue'
 import { useForm } from '@inertiajs/vue3'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAdd, faAddressBook, faBook, faFilm, faTv, faGamepad, faMusic, faVideo } from '@fortawesome/free-solid-svg-icons'
+import { faAdd, faAddressBook, faBook, faLeaf, faFilm, faTv, faGamepad, faMusic, faVideo,faArchway, faSprayCan, faBuilding, faMuseum, faUtensils } from '@fortawesome/free-solid-svg-icons'
 
 // Estado para mostrar u ocultar el formulario y para la categoría seleccionada
 const selectedCategory = ref(null)
@@ -26,7 +26,7 @@ function selectCategory(category) {
 
 const translateCategory = (categoryName) => {
     const translations = {
-        book: 'Libro',
+        nature: 'Naturaleza',
         movie: 'Película',
         game: 'Juego',
         serie: 'Serie',
@@ -56,6 +56,7 @@ function submitForm() {
         onSuccess: () => {
             form.reset()
             selectedCategory.value = null
+            console.log("GUardado")
         },
         data: formData,
         forceFormData: true,  // Asegura que los datos se envían como FormData
@@ -67,17 +68,7 @@ function goBack() {
     selectedCategory.value = null
 }
 
-const getPronoun = (category) => {
-  const feminineCategories = ['Película', 'Serie', 'Música', 'Documental', 'Podcast'];
-  const translatedCategory = translateCategory(category); // Asegúrate de que la categoría esté traducida
-  return feminineCategories.includes(translatedCategory) ? 'la' : 'lo';
-};
 
-const getPronoun2 = (category) => {
-  const feminineCategories = ['Película', 'Serie', 'Música'];
-  const translatedCategory = translateCategory(category); // Asegúrate de que la categoría esté traducida
-  return feminineCategories.includes(translatedCategory) ? 'de la' : 'del';
-};
 </script>
 
 <template>
@@ -90,54 +81,42 @@ const getPronoun2 = (category) => {
         <div v-if="!selectedCategory" class="p-6 bg-white dark:bg-gray-800 rounded-md shadow-md text-center">
             <p class="text-lg mb-4">Selecciona una categoría:</p>
             <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-                <div @click="selectCategory('book')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'book']"
+                <div @click="selectCategory('nature')" class="cursor-pointer text-center">
+                    <font-awesome-icon :icon="['fas', 'leaf']"
                         class="text-blue-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Libros</p>
+                    <p class="mt-2 text-sm">Naturaleza</p>
                 </div>
-                <div @click="selectCategory('movie')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'film']"
+                <div @click="selectCategory('monument')" class="cursor-pointer text-center">
+                    <font-awesome-icon :icon="['fas', 'archway']"
                         class="text-green-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Películas</p>
+                    <p class="mt-2 text-sm">Monumentos</p>
                 </div>
-                <div @click="selectCategory('serie')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'tv']" class="text-red-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Series</p>
+                <div @click="selectCategory('streetart')" class="cursor-pointer text-center">
+                    <font-awesome-icon :icon="['fas', 'spray-can']" class="text-red-500 h-16 w-16 mx-auto"></font-awesome-icon>
+                    <p class="mt-2 text-sm">Street Art</p>
                 </div>
-                <div @click="selectCategory('game')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'gamepad']"
+                <div @click="selectCategory('architecture')" class="cursor-pointer text-center">
+                    <font-awesome-icon :icon="['fas', 'building']"
                         class="text-purple-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Juegos</p>
+                    <p class="mt-2 text-sm">Arquitectura</p>
                 </div>
-                <div @click="selectCategory('music')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'music']"
+                <div @click="selectCategory('library')" class="cursor-pointer text-center">
+                    <font-awesome-icon :icon="['fas', 'book']"
                         class="text-yellow-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Música</p>
+                    <p class="mt-2 text-sm">Bibliotecas</p>
                 </div>
 
-                <!-- Nueva categoría: Videos -->
-                <div @click="selectCategory('video')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'circle-play']"
+                <div @click="selectCategory('museum')" class="cursor-pointer text-center">
+                    <font-awesome-icon :icon="['fas', 'museum']"
                         class="text-pink-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Videos</p>
+                    <p class="mt-2 text-sm">Museos</p>
                 </div>
-                <!-- Nueva categoría: Destinos Turísticos -->
-                <div @click="selectCategory('destination')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'location-dot']"
+                <div @click="selectCategory('piacada')" class="cursor-pointer text-center">
+                    <font-awesome-icon :icon="['fas', 'utensils']"
                         class="text-orange-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Destinos Turísticos</p>
+                    <p class="mt-2 text-sm">Picadas</p>
                 </div>
-                <!-- Otras posibles categorías -->
-                <div @click="selectCategory('documentary')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'file-video']"
-                        class="text-teal-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Documentales</p>
-                </div>
-                <div @click="selectCategory('podcast')" class="cursor-pointer text-center">
-                    <font-awesome-icon :icon="['fas', 'podcast']"
-                        class="text-purple-500 h-16 w-16 mx-auto"></font-awesome-icon>
-                    <p class="mt-2 text-sm">Podcasts</p>
-                </div>
+
             </div>
         </div>
 
@@ -158,14 +137,8 @@ const getPronoun2 = (category) => {
                 <div class="mb-4">
                     
                     <label for="description" class="block text-sm font-bold text-gray-900 dark:text-gray-300 ">
-                    ¿Qué te gustó {{ getPronoun2(selectedCategory) }} {{translateCategory(selectedCategory)}}?
+                    Descripción
                 </label>                                        
-                   <label>
-                ¿Por qué <span>{{ getPronoun(selectedCategory) }}</span> recomendarías? Otra persona te lo agradecerá.
-                </label>
-
-                    
-
 
                     <textarea v-model="form.description" id="description" rows="4"
                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-700 dark:text-gray-300"
