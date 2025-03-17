e7a25df103b8cc79c9e29a1b66433cf6d94a99e0
Cesar-eav <cesar.eav@hotmail.es>
2025-03-17 11:42:41 -0300

    Diseño en filas warp

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -9,8 +9,7 @@
         </template>
 
         <!-- Contenido principal -->
-        <div
-            class="CONTENIDO md:p-6 p-0 bg-white dark:bg-gray-800 rounded-md shadow-md flex justify-between mt-10 md:mt-3">
+        <div class="CONTENIDO md:p-6 p-0 bg-white rounded-md shadow-md flex justify-between mt-10 md:mt-3">
             <div class="w-full md:mr-4 ">
                 <!-- Filtros de categorías -->
 
@@ -43,84 +42,31 @@
                         <FontAwesomeIcon :icon="faUtensils" class="text-gray-400 text-2xl hover:text-[#3c888d]" />
                         <span class="text-sm text-gray-600 dark:text-gray-400">Picadas</span>
                     </div>
-            </div>
+                </div>
                 <!-- Posteos de otros usuarios -->
-                <div v-for="(post, index) in filteredPosts" :key="index"
-                    class="bg-gray-100 dark:bg-gray-700 p-4 rounded-lg shadow-md mb-6 flex flex-col md:flex-row">
-                    <img :src="post.image ? `/storage/${post.image}` : '/storage/sin-portada.jpg'"
-                        alt="Recommendation Image"
-                        class="w-full h-48 object-cover rounded-md mb-4 md:w-24 md:h-32 md:mr-4 md:mb-0">
+                <div class="flex flex-wrap w-full gap-1 mx-5 justify-center ">
+                    <div v-for="(post, index) in filteredPosts" :key="index"
+                        class="bg-gray-100 dark:bg-gray-700 p-4 rounded-lg shadow-md mb-6 flex flex-col w-96 md:w-56">
+                        <img :src="post.image ? `/storage/${post.image}` : '/storage/sin-portada.jpg'"
+                        alt="Recommendation Image" class="h-36 w-full object-cover rounded-md mb-4 md:mr-4 md:mb-0">
 
-                    <div class="flex flex-col w-full">
-                        <div class="flex flex-col md:flex-row items-start md:items-center mb-2">
+                        <div class="flex flex-row  items-start md:items-center mb-2">
                             <FontAwesomeIcon :icon="getCategoryIcon(post.category)" class="mr-2 text-gray-400" />
                             <h4 class="text-md font-bold text-gray-800 dark:text-gray-300">{{ post.title }}</h4>
                         </div>
                         <div class="inline-flex">
 
                             <p class="text-sm text-gray-600 dark:text-gray-400 mb-2 break-words">
-                                <strong class="text-gray-800 text-2xl">"</strong>
+
                                 {{ isExpanded[index] ? post.description : post.description.slice(0, 350) }}
-                                <strong class="text-gray-800 text-xl">"</strong>
 
                                 <span v-if="post.description.length > 350" @click="toggleExpansion(index)"
                                     class="text-blue-500 cursor-pointer">
                                     {{ isExpanded[index] ? '...ver menos' : '...ver más' }}
                                 </span>
-
-
                             </p>
                         </div>
-                        <div class="text-sm text-gray-800 dark:text-gray-300 mb-2">
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center flex-shrink-0">
-                                    <img :src="post.user && post.user.profile_picture ? 'storage/' + post.user.profile_picture : '/storage/images/Sin-perfil.jpg'"
-                                        class="rounded-full w-8 h-8 mr-2 ">
-
-                                    <Link :href="route('social.profile', post.user.id)"
-                                        class="text-blue-500 hover:underline">
-                                    {{ post.user ? post.user.name : 'Invitado' }}
-                                    </Link>
-                                </div>
-                                <button @click="toggleFollow(post.user.id, index)"
-                                    :class="isFollowing[index] ? 'bg-[#3c888d]' : 'bg-[#000000]'"
-                                    class="text-white px-4 py-1 rounded-lg text-sm min-w-[110px] text-center">
-                                    {{ isFollowing[index] ? 'Siguiendo' : 'Seguir' }}
-                                </button>
-                            </div>
-                        </div>
-
-                        <!-- Sección de comentarios -->
-                        <div class="mt-4">
-                            <h5 class="text-lg font-semibold text-gray-800 dark:text-gray-300">Comentarios</h5>
-
-                            <!-- Mostrar los comentarios existentes -->
-                            <div v-for="comment in post.comments" :key="comment.id"
-                                class="mt-2 p-2 bg-gray-200 dark:bg-gray-600 rounded-md">
-                                <div class="inline-flex items-center">
-                                    <img :src="comment.user.profile_picture ? 'storage/' + comment.user.profile_picture : '/storage/images/Sin-perfil.jpg'"
-                                        class="rounded-full w-8 h-8 mr-2 ">
-
-                                    <p class="text-sm text-gray-700 dark:text-gray-200"><strong>{{ comment.user.name
-                                            }}:</strong>
-                                    </p>
-                                </div>
-                                <p> {{ comment.comment }}</p>
-                            </div>
-
-                            <!-- Formulario para agregar un nuevo comentario -->
-                            <div class="mt-2">
-                                <textarea v-model="newComment[index]" rows="2" placeholder="Escribe un comentario..." @click="handleCommentClick"
-                                    class="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"></textarea>
-                                <button @click="submitComment(post.id, index)"
-                                    class="mt-2 px-4 py-1 bg-blue-500 text-white rounded-lg">
-                                    Comentar
-                                </button>
-                            </div>
-                        </div>
                     </div>
-
-
                 </div>
 
             </div>
@@ -142,7 +88,8 @@
                         </button>
                     </div>
                     <pre>CATEGORY: {{ categoryName }}</pre>
-                    <div class="bg-gray-900 p-3 rounded-md shadow-md"><p class="text-white">asdsad</p>
+                    <div class="bg-gray-900 p-3 rounded-md shadow-md">
+                        <p class="text-white">asdsad</p>
                         <div v-if="Array.isArray(category)" v-for="recommendation in category.slice(0, 3)"
                             :key="recommendation.id" class="mb-4">asdasdsssasd
                             <h4 class="text-md font-bold text-white">{{ recommendation.title }}</h4>
@@ -167,7 +114,7 @@
 import { ref, computed, onMounted, nextTick } from 'vue';
 import AuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { usePage, router, Link } from '@inertiajs/vue3';
-import { faSyncAlt, faUser, faBook, faFilm, faGamepad, faTv, faMusic, faCirclePlay, faFileVideo, faLocationDot, faPodcast,faLeaf,faArchway, faSprayCan, faBuilding, faMuseum, faUtensils} from '@fortawesome/free-solid-svg-icons';
+import { faSyncAlt, faUser, faBook, faFilm, faGamepad, faTv, faMusic, faCirclePlay, faFileVideo, faLocationDot, faPodcast, faLeaf, faArchway, faSprayCan, faBuilding, faMuseum, faUtensils } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import axios from 'axios';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -272,10 +219,17 @@ const toggleExpansion = (index) => {
 const getCategoryIcon = (categoryName) => {
     const icons = {
         nature: faLeaf,
+        monument: faArchway,
+        streetart: faSprayCan,
+        architecture: faBuilding,
+        library: faBook,
+        museum: faMuseum,
+        picada: faUtensils,
         books: faBook,
         movies: faFilm,
         series: faTv,
-        music: faMusic
+        music: faMusic,
+
     };
     return icons[categoryName] || faBook;
 };
