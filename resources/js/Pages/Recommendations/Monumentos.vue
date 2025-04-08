f37f9821a23fb19ec7351ba52d280c1c3402f92b
Cesar-eav <cesar.eav@hotmail.es>
2025-04-08 10:58:38 -0400

    Pin GPS all views

--- resources/js/Pages/Recommendations/Monumentos.vue
+++ resources/js/Pages/Recommendations/Monumentos.vue
@@ -25,8 +25,10 @@
                                 class="text-red-500 text-md hover:text-red-400 mr-2" />
                             </a>
 
-                            <a class="block md:hidden" :href="`geo:${post.lat},${post.lng}?z=18`" target="_blank"> <font-awesome-icon icon="fa-solid fa-location-dot"
-                                class="text-red-500 text-md hover:text-red-400 mr-2" /> </a>
+                            <a class="block md:hidden" :href="`geo:${post.lat},${post.lng}?q=${post.lat},${post.lng}&z=18`" target="_blank"> 
+                                <font-awesome-icon icon="fa-solid fa-location-dot"
+                                class="text-red-500 text-md hover:text-red-400 mr-2" /> 
+                            </a>
 
 
                             <h4 class="text-md font-bold text-[#3c888d] dark:text-gray-300">{{ post.title }}</h4>
